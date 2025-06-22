495ce7a02d1861696fa7b109054ada19d1bc037c
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-22 14:14:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzn2FEor1rOwDg1msDe-RbiLl0TcuZhnGMIsyOpa38k2QY4Nr3MiukIMUHnuBWfrYqoaQ/exec', // ❗ ใส่ URL ที่ได้จากการ Deploy
+  LIFF_ID: '2007514355-PRvqxKwk',                       // ❗ ใส่ LIFF ID ของคุณ
+  ADMIN_SECRET: 'YourSuperSecretKey123'          // ❗ ใส่รหัสลับให้ตรงกับในชีต Settings
+};
