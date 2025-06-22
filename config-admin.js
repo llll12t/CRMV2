23b80140e64cc5ee38bf9c57d241b2b2881414bb
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-22 17:52:00 +0700

    Create config-admin.js

---
+++ config-admin.js
@@ -0,0 +1,10 @@
+// config-admin.js
+
+const CONFIG = {
+    /**
+     * URL ของ Google Apps Script Web App ที่ถูก Deploy สำหรับหน้า Admin
+     * ต้องเป็น URL ที่ได้จากการ Deploy สคริปต์ของ Admin โดยเฉพาะ
+     * ที่มีฟังก์ชันสำหรับ Admin เช่น getUsers, getAllHistory, updateUserAdmin ฯลฯ
+     */
+    GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycbw7dfgZMuR-le-2TSiQ1hf90Q4W2thJq55FWeFVKjfpJE9e4Jo3Zt91dAIA2HSfqP-yEw/exec' // <--- !!! วาง URL ที่ได้จากการ Deploy สคริปต์ของ Admin ที่นี่ !!!
+};
