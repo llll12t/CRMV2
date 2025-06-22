3b3bbeed17d61a181aa302d6bb090995de2dea85
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-22 15:59:32 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzn2FEor1rOwDg1msDe-RbiLl0TcuZhnGMIsyOpa38k2QY4Nr3MiukIMUHnuBWfrYqoaQ/exec',
+  LIFF_ID: '2007514355-p1BnW75v'
+};
